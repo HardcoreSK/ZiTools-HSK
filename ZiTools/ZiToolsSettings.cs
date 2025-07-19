1c771d7f3d268d395ca649ed73a668497728f5a6
Lightwave <thelonerd@gmail.com>
2025-07-19 19:17:05 +0400

    Expose ModSettings

--- ZiTools/ZiToolsSettings.cs
+++ ZiTools/ZiToolsSettings.cs
@@ -8,5 +8,14 @@ namespace ZiTools
         public bool showArrowsToSelectedObject = true;
         public int maxArrowsInScreen = 100;
         public bool arrowsOffscreenOnly = true;
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look(ref hideOldMapSearchIcon, "HideOldMapSearchIcon", true);
+            Scribe_Values.Look(ref showArrowsToSelectedObject, "ShowArrowsToSelectedObject", true);
+            Scribe_Values.Look(ref maxArrowsInScreen, "MaxArrowsInScreen", 100);
+            Scribe_Values.Look(ref arrowsOffscreenOnly, "ArrowsOffscreenOnly", true);
+        }
     }
 }
